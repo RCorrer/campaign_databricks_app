c6a919add97efe17d6d03de1600a55a38d96620e
RCorrer <rafael_correr@hotmail.com>
2026-04-20 01:23:25 -0300

    ajuste

--- frontend/src/pages/SegmentationPage.jsx
+++ frontend/src/pages/SegmentationPage.jsx
@@ -1,4 +1,5 @@
-import { Link, useState } from 'react-router-dom'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import RuleGroupCard from '../components/RuleGroupCard'
 
 export default function SegmentationPage({ campaign, topics }) {
