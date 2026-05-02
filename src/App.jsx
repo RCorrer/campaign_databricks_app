1378e5bb0fb551025e0d84695eb37d4ce1f252ad
RCorrer <rafael_correr@hotmail.com>
2026-05-01 22:52:39 -0300

    ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import DashboardPage from './pages/DashboardPage'
-import CampaignDetail from './pages/CampaignDetail'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import CampaignForm from './pages/CampaignForm'
+import Segmentation from './pages/Segmentation'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/campaigns/:id" element={<CampaignDetail />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/campaign/new" element={<CampaignForm />} />
+        <Route path="/campaign/:id" element={<CampaignForm />} />
+        <Route path="/segmentation/:id" element={<Segmentation />} />
+      </Routes>
+    </Router>
   )
 }
